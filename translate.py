456ba8f75f37109f0621be61eb5da941dbd2070b
root <root@louwang-VirtualBox.(none)>
2014-04-03 15:56:50 +0800

    regexp basic

--- translate.py
+++ translate.py
@@ -2,25 +2,65 @@
 
 import re
 import sys
-import json
+
+target = "abc123 kkk444 342342 3123123 231313  wtf434"
 
 
+def match(pattern):
+	typelist = []
+	opflag = 0
+	typemap = {0:"[a-z]",1:"[A-Z]",2:"\d",3:"\S",4:"\s"}
+	for i in pattern:
+		if i == '[':
+			opflag += 1
+		if i == ']':
+			opflag -= 1
+		if opflag >= 1:
+			if opflag == 1 and i == '[':
+				continue
+			else:
+				typelist.append(i)
+			print 'specific'
+			#enter specific word check mode
+		else:
+			if opflag == 0 and i == '[' or i == ']':
+				continue
+			#normal symobic mode
+			if ord(i) >= 97 and ord(i) <= 122:
+					typelist.append(0)
+			elif ord(i) >= 65 and ord(i) <= 90:
+					typelist.append(1)
+			elif ord(i) >= 48 and ord(i) <= 57:
+					typelist.append(2)
+			elif ord(i) >= 33 and ord(i) <= 47 or ord(i) >= 58 and ord(i) <= 64 or ord(i) >= 91 and ord(i) <= 96 or ord(i) >= 123 and ord(i) <= 126:
+					typelist.append(3)
+			else:
+					typelist.append(4)
+		print i, opflag
+	regexstr = ""
+	for j in typelist:
+		if type(j) is str:
+			regexstr += j
+		else:
+			regexstr += typemap[j]
+	return '('+regexstr+')'
 
-def testf(pat):
-	print pat
-	for i in pat:
-		a = ord(i)
-		print a
 
-print '''aasdfdsfa1232131232131'dfdfdfd'dfdf'''
+#print json.dumps()
 
+#outputre = match('[[][\/+]~!@#$%^&''""**()_+}|]')
+outputre = match('def555')
+print outputre
 
-print json.dumps()
+s = outputre
+m = re.findall(s, target)
+print m
 
-testf('abc'abc'')
-'a+[aa'']'
-#0: char
-#1: digit
-#2: symbol
-#3: space
+#quote need to escape =.=
+#ascii decimal number
+#0: char : 97 ~ 122 (lowercase)
+#1: 65 ~ 90 (uppercase)
+#2: digit : 48 ~ 57
+#3: symbol : 33 ~ 47 , 58 ~ 64, 91 ~ 96 , 123 ~ 126
+#4: space : 32
 
