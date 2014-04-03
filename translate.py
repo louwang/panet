cc4583b599753ee3c863a906bd9588826ff6ef85
louwang <louwang@louwang-VirtualBox.(none)>
2014-04-03 10:28:13 +0800

    add ascii code idea

---
+++ translate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import json
+
+
+
+def testf(pat):
+	print pat
+	for i in pat:
+		a = ord(i)
+		print a
+
+print '''aasdfdsfa1232131232131'dfdfdfd'dfdf'''
+
+
+print json.dumps()
+
+testf('abc'abc'')
+'a+[aa'']'
+#0: char
+#1: digit
+#2: symbol
+#3: space
+
